ef21f39cc0857d3ebddf220c2bd458355d9db0fb
Matsue Kenta <40712363+matsuei@users.noreply.github.com>
2023-03-18 21:17:53 +0900

    Initial Commit

---
+++ ChatGPTPlayground/ChatGPTPlaygroundApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChatGPTPlaygroundApp.swift
+//  ChatGPTPlayground
+//
+//  Created by Kenta Matsue on 2023/03/18.
+//
+
+import SwiftUI
+
+@main
+struct ChatGPTPlaygroundApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
